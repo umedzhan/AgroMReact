03a331dd5054408f91b1e0044d575b68ccba341d
umedzhan <umedzhan@kali.kali>
2026-02-10 23:17:14 +0500

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css';
 import './i18n'; // Import i18n config
 import App from './App.jsx';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://api.agrom24.uz';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
